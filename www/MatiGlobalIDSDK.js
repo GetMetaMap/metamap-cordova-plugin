3bbbd43080f7ff38803584732f5bf4e7b240675d
andrey.posnov <andrey.posnov@mati.io>
2021-06-08 17:21:53 +0300

    fix typo

--- www/MatiGlobalIDSDK.js
+++ www/MatiGlobalIDSDK.js
@@ -1,7 +1,5 @@
    var exec = require("cordova/exec");
 
-    var exec = require("cordova/exec");
-
     exports.coolMethod = function(arg0, success, error) {
       exec(success, error, "MatiGlobalIDSDK", "coolMethod", [arg0]);
     };
