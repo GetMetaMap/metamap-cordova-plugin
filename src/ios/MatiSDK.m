e26d1794c7577f9cbb2a32dde783f906ac52fe76
Andrew Mykytiuk <andrew.mykytiuk@mati.io>
2022-01-13 14:18:32 +0200

    Update verificationID method

--- src/ios/MatiSDK.m
+++ src/ios/MatiSDK.m
@@ -80,7 +80,8 @@
 
 - (void)verificationSuccessWithIdentityId:(NSString *)identityId verificationID:(nullable NSString *)verificationID {
     if(setMatiCallbackCDVInvokedUrlCommand != nil){
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:identityId];
+        NSDictionary *dict = @{@"identityId": identityId, @"verificationID": verificationID};
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dict];
         [pluginResult setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:setMatiCallbackCDVInvokedUrlCommand.callbackId];
     }
