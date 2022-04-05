0442c8aeb03ebf25c3b03bd3705f642c7b0c8db6
Аво <avetik.sukiasyan@mati.io>
2022-04-05 12:26:35 +0400

    fixed issue

--- www/MatiGlobalIDSDK.js
+++ www/MatiGlobalIDSDK.js
@@ -1,11 +1,11 @@
    var exec = require("cordova/exec");
 
     exports.setMetaMapCallback = function(success, error) {
-        exec(success, error, "MetaMapGlobalIDSDK", "setMetaMapCallback", []);
+        exec(success, error, "MatiGlobalIDSDK", "setMetaMapCallback", []);
     }
 
     exports.showMetaMapFlow = function(params, success, error) {
       var { metadata } = params
       params = { ...params, metadata: {...metadata, sdkType: "cordova"}}
-      exec(success, error, "MetaMapGlobalIDSDK", "showMetaMapFlow", [params]);
+      exec(success, error, "MatiGlobalIDSDK", "showMetaMapFlow", [params]);
     }
